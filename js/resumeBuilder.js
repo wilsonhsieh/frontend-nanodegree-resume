7c89c87c690824db150b58128a63b8df577f09f4
Wilson Hsieh <wilsonhsieh@users.noreply.github.com>
2015-01-05 21:11:43 -0600

    Add name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,13 @@
-$("#main").append("Wilson Hsieh");
+/* $("#main").append("Wilson Hsieh");
+var awesomeThoughts = "I am Wilson and I am AWESOME";
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "fun");
+
+console.log(funThoughts);
+*/
+
+var formattedName = HTMLheaderName.replace("%data%", "Wilson Hsieh");
+$("#header").append(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", "Front End Web Developer");
+$("#header").append(formattedRole);
